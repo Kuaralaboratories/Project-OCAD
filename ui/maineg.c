1ca8b09050f652f9f5a2109f1acccca71f079d04
yakupcemilk <kayabasyakupcemil@gmail.com>
2024-07-30 16:43:00 +0300

    MicroUI needs Added

---
+++ ui/maineg.c
@@ -0,0 +1,161 @@
+/**
+ * @file    maineg.c
+ * @author  Yakup Cemil Kayabaş (ceoyakupcemil@kuaralabs.org)
+ * @brief   This code is for research about sidebar of OpenCAD
+ * @date    2024-07-30
+ */
+
+#include <SDL2/SDL.h>
+#include <stdio.h>
+#include "renderer.h"
+#include "microui.h"
+
+static  char logbuf[64000];
+static   int logbuf_updated = 0;
+static float bg[3] = { 90, 95, 100 };
+
+/* Needed for logging */
+static void write_log(const char *text) {
+  if (logbuf[0]) { strcat(logbuf, "\n"); }
+  strcat(logbuf, text);
+  logbuf_updated = 1;
+}
+
+/* Slider template code */
+static int uint8_slider(mu_Context *ctx, unsigned char *value, int low, int high) {
+  static float tmp;
+  mu_push_id(ctx, &value, sizeof(value));
+  tmp = *value;
+  int res = mu_slider_ex(ctx, &tmp, low, high, 0, "%.0f", MU_OPT_ALIGNCENTER);
+  *value = tmp;
+  mu_pop_id(ctx);
+  return res;
+}
+
+/* Log Window is for the  */
+static void log_window(mu_Context *ctx) {
+  if (mu_begin_window(ctx, "Log Window", mu_rect(350, 40, 300, 200))) {
+    /* output text panel */
+    mu_layout_row(ctx, 1, (int[]) { -1 }, -25);
+    mu_begin_panel(ctx, "Log Output");
+    mu_Container *panel = mu_get_current_container(ctx);
+    mu_layout_row(ctx, 1, (int[]) { -1 }, -1);
+    mu_text(ctx, logbuf);
+    mu_end_panel(ctx);
+    if (logbuf_updated) {
+      panel->scroll.y = panel->content_size.y;
+      logbuf_updated = 0;
+    }
+
+    /* input textbox + submit button */
+    static char buf[128];
+    int submitted = 0;
+    mu_layout_row(ctx, 2, (int[]) { -70, -1 }, 0);
+    if (mu_textbox(ctx, buf, sizeof(buf)) & MU_RES_SUBMIT) {
+      mu_set_focus(ctx, ctx->last_id);
+      submitted = 1;
+    }
+    if (mu_button(ctx, "Submit")) { submitted = 1; }
+    if (submitted) {
+      write_log(buf);
+      buf[0] = '\0';
+    }
+
+    mu_end_window(ctx);
+  }
+}
+
+
+static void process_frame(mu_Context *ctx) {
+  mu_begin(ctx);
+  style_window(ctx);
+  log_window(ctx);
+  test_window(ctx);
+  mu_end(ctx);
+}
+
+static const char button_map[256] = {
+  [ SDL_BUTTON_LEFT   & 0xff ] =  MU_MOUSE_LEFT,
+  [ SDL_BUTTON_RIGHT  & 0xff ] =  MU_MOUSE_RIGHT,
+  [ SDL_BUTTON_MIDDLE & 0xff ] =  MU_MOUSE_MIDDLE,
+};
+
+static const char key_map[256] = {
+  [ SDLK_LSHIFT       & 0xff ] = MU_KEY_SHIFT,
+  [ SDLK_RSHIFT       & 0xff ] = MU_KEY_SHIFT,
+  [ SDLK_LCTRL        & 0xff ] = MU_KEY_CTRL,
+  [ SDLK_RCTRL        & 0xff ] = MU_KEY_CTRL,
+  [ SDLK_LALT         & 0xff ] = MU_KEY_ALT,
+  [ SDLK_RALT         & 0xff ] = MU_KEY_ALT,
+  [ SDLK_RETURN       & 0xff ] = MU_KEY_RETURN,
+  [ SDLK_BACKSPACE    & 0xff ] = MU_KEY_BACKSPACE,
+};
+
+static int text_width(mu_Font font, const char *text, int len) {
+  if (len == -1) { len = strlen(text); }
+  return r_get_text_width(text, len);
+}
+
+static int text_height(mu_Font font) {
+  return r_get_text_height();
+}
+
+int main(int argc, char **argv) {
+  /* init SDL and renderer */
+  SDL_Init(SDL_INIT_EVERYTHING);
+  r_init();
+
+  /* init microui */
+  mu_Context *ctx = malloc(sizeof(mu_Context));
+  mu_init(ctx);
+  ctx->text_width = text_width;
+  ctx->text_height = text_height;
+
+  /* main loop */
+  for (;;) {
+    /* handle SDL events */
+    SDL_Event e;
+    while (SDL_PollEvent(&e)) {
+      switch (e.type) {
+        case SDL_QUIT: exit(EXIT_SUCCESS); break;
+        case SDL_MOUSEMOTION: mu_input_mousemove(ctx, e.motion.x, e.motion.y); break;
+        case SDL_MOUSEWHEEL: mu_input_scroll(ctx, 0, e.wheel.y * -30); break;
+        case SDL_TEXTINPUT: mu_input_text(ctx, e.text.text); break;
+
+        case SDL_MOUSEBUTTONDOWN:
+        case SDL_MOUSEBUTTONUP: {
+          int b = button_map[e.button.button & 0xff];
+          if (b && e.type == SDL_MOUSEBUTTONDOWN) { mu_input_mousedown(ctx, e.button.x, e.button.y, b); }
+          if (b && e.type ==   SDL_MOUSEBUTTONUP) { mu_input_mouseup(ctx, e.button.x, e.button.y, b);   }
+          break;
+        }
+
+        case SDL_KEYDOWN:
+        case SDL_KEYUP: {
+          int c = key_map[e.key.keysym.sym & 0xff];
+          if (c && e.type == SDL_KEYDOWN) { mu_input_keydown(ctx, c); }
+          if (c && e.type ==   SDL_KEYUP) { mu_input_keyup(ctx, c);   }
+          break;
+        }
+      }
+    }
+
+    /* process frame */
+    process_frame(ctx);
+
+    /* render */
+    r_clear(mu_color(bg[0], bg[1], bg[2], 255));
+    mu_Command *cmd = NULL;
+    while (mu_next_command(ctx, &cmd)) {
+      switch (cmd->type) {
+        case MU_COMMAND_TEXT: r_draw_text(cmd->text.str, cmd->text.pos, cmd->text.color); break;
+        case MU_COMMAND_RECT: r_draw_rect(cmd->rect.rect, cmd->rect.color); break;
+        case MU_COMMAND_ICON: r_draw_icon(cmd->icon.id, cmd->icon.rect, cmd->icon.color); break;
+        case MU_COMMAND_CLIP: r_set_clip_rect(cmd->clip.rect); break;
+      }
+    }
+    r_present();
+  }
+
+  return 0;
+}
